bccaa4d3bdbf751e270500743444fb35b39352fa
W1ndst0rm <levi.jumonville@gmail.com>
2020-10-04 05:31:59 -0600

    Add tests for RateLimiter, TokenManager, ConnectionManagaer, and get_credentials

---
+++ filevine-tests/test_get_credentials.py
@@ -0,0 +1,51 @@
+import unittest
+import os
+from filevine import get_credentials, FilevineException
+
+
+class TestCredentialImport(unittest.TestCase):
+    @staticmethod
+    def get_credentials(test_cred_data):
+        creds_file = 'creds.yml'
+        with open(creds_file, 'w') as out:
+            for k in test_cred_data:
+                out.write(f"{k}: {test_cred_data[k]}\n")
+        try:
+            credentials = get_credentials(creds_file)
+            return credentials
+        finally:
+            os.remove(creds_file)
+
+    def test_success(self):
+        creds_data = {
+            'key': 'fvpk_00000000-0000-0000-0000-000000000000',
+            'secret': 'fvsk_0000000000000000000000000000000000000000000000000000000000000000',
+            'queueid': 'q12345678901'
+
+        }
+        credentials = self.get_credentials(creds_data)
+        self.assertEqual(credentials.key, creds_data['key'])
+        self.assertEqual(credentials.secret, creds_data['secret'])
+        self.assertEqual(credentials.queueid, creds_data['queueid'])
+
+    def test_missing_key(self):
+        creds_data = {
+            'secret': 'fvsk_0000000000000000000000000000000000000000000000000000000000000000',
+            'queueid': 'q12345678901'
+
+        }
+        with self.assertRaises(FilevineException):
+            self.get_credentials(creds_data)
+
+    def test_missing_secret(self):
+        creds_data = {
+            'key': 'fvpk_00000000-0000-0000-0000-000000000000',
+            'queueid': 'q12345678901'
+
+        }
+        with self.assertRaises(FilevineException):
+            self.get_credentials(creds_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
